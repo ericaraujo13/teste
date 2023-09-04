98bb98c50a1d52cd00ca12f4265c6a30a4b12c99
ericaruajo13 <ericpoloni@gmail.com>
2023-09-04 14:46:09 -0300

    Fix bugs and create methods

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,10 @@
 class Document < ApplicationRecord
   has_one_attached :file
-  validates :file_name, presence: true, length: { maximum: 250 }
-  validates :path, presence: true, length: { maximum: 250 }
+
+  after_commit :update_file_info, on: :create
+
+  def update_file_info
+    new_path = ActiveStorage::Blob.service.path_for(file.key)
+    update(path: new_path, file_name: file.blob.filename)
+  end
 end
