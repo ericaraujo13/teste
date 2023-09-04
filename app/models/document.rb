0a854d1c72fcac0afcb98f455ce803209fa2e154
ericaruajo13 <ericpoloni@gmail.com>
2023-09-04 12:30:30 -0300

    test document model valdiations

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,5 @@
 class Document < ApplicationRecord
+  has_one_attached :file
+  validates :file_name, presence: true, length: { maximum: 250 }
+  validates :path, presence: true, length: { maximum: 250 }
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Document, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it { is_expected.to validate_presence_of(:file_name) }
+    it { is_expected.to validate_length_of(:file_name).is_at_most(250) }
+    it { is_expected.to validate_presence_of(:path) }
+    it { is_expected.to validate_length_of(:path).is_at_most(250) }
+  end
 end
